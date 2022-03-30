e6739f5f8925ea3e364757f66517537a82fdb3e2
hdk325 <hdk325@naver.com>
2022-03-30 15:56:02 +0900

    객체지향쿼리언어

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,8 +4,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
-import java.util.Set;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -17,34 +19,18 @@ public class JpaMain {
         tx.begin();
 
         try {
+
             Member member = new Member();
             member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity","street","10000"));
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1","street","10000"));
-            member.getAddressHistory().add(new AddressEntity("old2","street","10000"));
-
             em.persist(member);
 
 
-            em.flush();
-            em.clear();
+            List<Member> resultList = em.createNativeQuery("select MEMBER_ID,city,street,zipcode,USERNAME from MEMBER", Member.class)
+                    .getResultList();
 
-            System.out.println("================ START ===================");
-            Member findMember = em.find(Member.class, member.getId());
-//            Address a = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("newCity",a.getStreet(),a.getZipcode()));
-//
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("한식");
-
-//            findMember.getAddressHistory().remove(new Address("old1","street","10000"));
-//            findMember.getAddressHistory().add(new Address("newCity1","street","10000"));
-
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1.getUsername());
+            }
 
             tx.commit();
         } catch (Exception e) {
